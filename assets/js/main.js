1a9a27b327e19767f79d1b99eb0973831bb548f3
Almog Arazi <112971847+Almog-Arazi@users.noreply.github.com>
2023-11-08 13:06:33 +0200

    first commit
    test Story boo

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+/*=============== SEARCH ===============*/
+
+
+/*=============== LOGIN ===============*/
+
+
+/*=============== ADD SHADOW HEADER ===============*/
+
+
+/*=============== HOME SWIPER ===============*/
+
+let swiperHome = new Swiper('.home__swiper', {
+    loop: true,
+    spaceBetween: -12,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
+
+  autoplay: {
+   delay: 3000,
+   disableOnInteraction: false,
+  },
+
+   breakpoints: {
+   720: {
+    spaceBetween: +32,
+  }
+ }
+});
+
+/*=============== FEATURED SWIPER ===============*/
+
+
+/*=============== NEW SWIPER ===============*/
+
+
+/*=============== TESTIMONIAL SWIPER ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
